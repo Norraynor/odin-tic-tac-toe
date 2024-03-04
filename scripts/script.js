bcc3206c0adcf53a7a9d28296ac270965cbd9e78
norraynor <37270508+Norraynor@users.noreply.github.com>
2024-03-04 20:18:29 +0100

    Add function to mark the gameboard

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 
-const playerOne = createPlayer('bob', 'X');
 
 const gameboard = (function createGameboard() {
     function createBoard(rows = 3, cols = 3) {
@@ -14,13 +13,27 @@ const gameboard = (function createGameboard() {
     }
     const board = createBoard();
     console.log(board);
+    function markBoard(posX,posY,player) {
+        board[posX][posY] = player.marker;
+    }
     //return obj
-    return {};
+    return {markBoard};
 })();
 
+const playerOne = createPlayer("bob", "X");
 function createPlayer(name, marker) {
-    console.log(name + ': ' +marker)
+    console.log(name + ': ' + marker)
     return {name,marker};
 }
 
+
 //controler to manage game
+const controller = (function createController() {
+    //control win conditions
+    //manage gameboard
+})();
+
+//player > plays round > next turn > opponent > plays round
+
+//test
+gameboard.markBoard(1, 1, playerOne);
